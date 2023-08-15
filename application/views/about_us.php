9033d67d9187098ea19f1ba1fe37c04bc55185aa
yulandi1 <yulandiputra@gmail.com>
2023-08-15 15:28:04 +0700

    first commit

---
+++ application/views/about_us.php
@@ -0,0 +1,8 @@
+<div class="container">
+  <div class="row">
+    <div class="col-md-12" style="margin-top: 60px;">
+      <center><h4 class="text-purple">About Us</h4></center>
+      <p>Nginep Yuk adalah sebuah web booking online yang menyediakan pelayanan sewa harian,bulanan dan tahunan untuk Apartemen,Hotel,dan Home stay. 
+       </div>
+  </div>
+</div>
---
+++ application/views/home.php
@@ -0,0 +1,76 @@
+<div class="jumbotron">
+	<h1 class="display-4">Selamat datang di <span class="text-purple text-bold">Nginep</span> Yuk.com</h1>
+	<p class="lead">Menyediakan berbagai tempat destinasi wisata dan penginapan</p>
+	<a class="btn btn-primary btn-lg" href="<?=base_url('order/apartements')?>" role="button">Pesan Sekarang</a>
+</div>
+<div class="container kota-list">
+	<div class="section-title"><span>Tersedia</span> di berbagai kota besar</div>
+	<div class="row">
+		<div class="col-md-4">
+			<div class="card">
+				<div class="card-body kota">
+					<center>
+					<h5 class="card-title">Jakarta</h5>
+					<img style="width: 200px" src="<?=base_url('assets')?>/images/jkt.png">
+					</center>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="card">
+				<div class="card-body kota">
+					<center>
+					<h5 class="card-title">Bandung</h5>
+					<img style="width: 200px" src="<?=base_url('assets')?>/images/bdg.png">
+					</center>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="card">
+				<div class="card-body kota">
+					<center>
+					<h5 class="card-title">Jogjakarta</h5>
+					<img style="width: 200px" src="<?=base_url('assets')?>/images/jgj.png">
+					</center>
+				</div>
+			</div>
+		</div>
+	</div>
+	<br>
+	<div class="row">
+		<div class="col-md-2">
+		</div>
+		<div class="col-md-3">
+			<div class="card">
+				<div class="card-body kota" style="height: 277px;">
+					<center>
+					<h5 class="card-title">Sumatera Utara</h5><br><br>
+					<img style="width: 200px" src="<?=base_url('assets')?>/images/smt.gif">
+					</center>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-3">
+			<div class="card">
+				<div class="card-body kota">
+					<center>
+					<h5 class="card-title">Malang</h5>
+					<img style="width: 200px" src="<?=base_url('assets')?>/images/mlg.png">
+					</center>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-3">
+			<div class="card">
+				<div class="card-body kota">
+					<center>
+					<h5 class="card-title">Nusa Tenggara Timur</h5>
+					<img style="width: 200px" src="<?=base_url('assets')?>/images/ntt.png">
+					</center>
+				</div>
+			</div>
+		<div class="col-md-2">
+		</div>
+	</div>
+</div>
